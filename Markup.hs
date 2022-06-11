770a041a52c5cdebc6f518de6966e3928801e11a
Jordan Butterwick <jordan@butterwick.tech>
2022-06-10 20:42:19 -0600

    more parsing markup

--- Markup.hs
+++ Markup.hs
@@ -1,12 +1,12 @@
--- Markup.hs
-
 module Markup
   ( Document
   , Structure(..)
+  , parse
   )
 where
 
 import Numeric.Natural
+import Data.Maybe (maybeToList)
 
 type Document
   = [Structure]
@@ -17,3 +17,58 @@ data Structure
   | UnorderedList [String]
   | OrderedList [String]
   | CodeBlock [String]
+  deriving (Eq, Show)
+
+
+parse :: String -> Document
+parse = parseLines Nothing . lines
+
+parseLines :: Maybe Structure -> [String] -> Document
+parseLines context txts =
+  case txts of
+
+    [] -> maybeToList context
+
+    ('*' : ' ' : line) : rest ->
+      maybe id (:) context (Heading 1 (trim line) : parseLines Nothing rest)
+
+    ('-' : ' ' : line) : rest ->
+      case context of
+        Just (UnorderedList list) ->
+          parseLines (Just (UnorderedList (list <> [trim line]))) rest
+
+        _ ->
+          maybe id (:) context (parseLines (Just (UnorderedList [trim line])) rest)
+
+    ('#' : ' ' : line) : rest ->
+      case context of
+        Just (OrderedList list) ->
+          parseLines (Just (OrderedList (list <> [trim line]))) rest
+
+        _ ->
+          maybe id (:) context (parseLines (Just (OrderedList [trim line])) rest)
+
+    ('>' : ' ' : line) : rest ->
+      case context of
+        Just (CodeBlock code) ->
+          parseLines (Just (CodeBlock (code <> [line]))) rest
+
+        _ ->
+          maybe id (:) context (parseLines (Just (CodeBlock [line])) rest)
+
+    currentLine : rest ->
+      let
+        line = trim currentLine
+      in
+        if line == ""
+          then
+            maybe id (:) context (parseLines Nothing rest)
+          else
+            case context of
+              Just (Paragraph paragraph) ->
+                parseLines (Just (Paragraph (unwords [paragraph, line]))) rest
+              _ ->
+                maybe id (:) context (parseLines (Just (Paragraph line)) rest)
+
+trim :: String -> String
+trim = unwords . words
