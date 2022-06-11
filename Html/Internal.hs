f4a8e4025633093fbdc40146261289a47288108f
Jordan Butterwick <jordan@butterwick.tech>
2022-06-10 20:40:59 -0600

    parsing markup

--- Html/Internal.hs
+++ Html/Internal.hs
@@ -82,3 +82,23 @@ escape =
         _ -> [c]
   in
     concat . map escapeChar
+
+parse :: String -> Document
+parse = parseLines [] . lines -- (1)
+
+parseLines :: [String] -> [String] -> Document
+parseLines currentParagraph txts =
+  let
+    paragraph = Paragraph (unlines (reverse currentParagraph)) -- (2), (3)
+  in
+    case txts of -- (4)
+      [] -> [paragraph]
+      currentLine : rest ->
+        if trim currentLine == ""
+          then
+            paragraph : parseLines [] rest -- (5)
+          else
+            parseLines (currentLine : currentParagraph) rest -- (6)
+
+trim :: String -> String
+trim = unwords . words
